eb8db3e224c224901acbe98adc9a92dd155a5740
kemlohko <alexkemloh@gmail.com>
2026-06-15 17:24:06 +0200

    fix: improve contrast in dark mode & rename style.css to styles.css

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const themeToggle = document.getElementById("theme-toggle");
 
 themeToggle.addEventListener("click", () => {
     const isDarkMode = document.body.getAttribute("data-theme") === "dark";
+    console.log("Current theme:", isDarkMode ? "Dark" : "Light");
     
     if(isDarkMode) {
+        console.log("Switching to light mode...");
         document.body.removeAttribute("data-theme");
     } else {
+        console.log("Switching to dark mode...");
         document.body.setAttribute("data-theme", "dark");
     }
 });
